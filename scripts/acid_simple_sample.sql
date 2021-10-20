0820bee056525e42881d222ab0acc8c9ee292922
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-20 15:28:40 +0200

    add acid sample

---
+++ scripts/acid_simple_sample.sql
@@ -0,0 +1,7 @@
+drop table if exists messages;
+create table messages (id int, message varchar(20));
+BEGIN;
+insert into messages values(1, 'Hello');
+insert into messages values(2, 'World');
+COMMIT;
+select * from messages;
