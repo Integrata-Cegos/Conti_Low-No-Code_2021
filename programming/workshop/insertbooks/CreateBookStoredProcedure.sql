e2d28bb8a95a71e6e1097d633d3c7fde9a8b1be8
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-04 13:45:24 +0100

    introduce ParseIsbn in CreateBook

--- programming/workshop/insertbooks/CreateBookStoredProcedure.sql
+++ programming/workshop/insertbooks/CreateBookStoredProcedure.sql
@@ -1,11 +1,17 @@
 -- use publishing
-CREATE PROCEDURE CreateBook @isbn1 int, @isbn2 int, @isbn3 int, @isbn4 int, @title varchar(200), @price float, @year int = 0, @subject varchar(20) = NULL, @category varchar(20) = NULL
+CREATE PROCEDURE CreateBook @isbnString varchar(20), @title varchar(200), @price float, @year int = 0, @subject varchar(20) = NULL, @category varchar(20) = NULL
 
 AS 
 
 BEGIN
     DECLARE @insertResult TABLE (generatedId numeric(19,0))
     DECLARE @generatedId numeric(19,0)
+    DECLARE @isbn1 int, @isbn2 int, @isbn3 int, @isbn4 int
+    select @isbn1 = value from ParseIsbn(@isbnString) where id = 1
+    select @isbn2 = value from ParseIsbn(@isbnString) where id = 2
+    select @isbn3 = value from ParseIsbn(@isbnString) where id = 3
+    select @isbn4 = value from ParseIsbn(@isbnString) where id = 4
+
 
     INSERT INTO PUBLISHING_BOOKS (isbn1, isbn2, isbn3, isbn4, price, title) OUTPUT INSERTED.id INTO @insertResult(generatedId) VALUES (@isbn1, @isbn2, @isbn3, @isbn4, @price, @title)
     select @generatedId = generatedId from @insertResult
--- programming/workshop/insertbooks/TestCreateBook.sql
+++ programming/workshop/insertbooks/TestCreateBook.sql
@@ -3,9 +3,9 @@ DELETE FROM PUBLISHING_SCHOOLBOOK where subject = 'sports'
 DELETE FROM PUBLISHING_SPECIALISTBOOK where category = 'sports'
 DELETE FROM PUBLISHING_BOOKS where isbn1 = 20
 
-EXEC CreateBook @isbn='20-11-12-14', @price=19.99,  @title='a simple sawitzki book'
-EXEC CreateBook @isbn='20-11-12-15', @price=19.99, @subject='sports', @year=10, @title='a simple sawitzki school book'
-EXEC CreateBook @isbn='20-11-12-16', @price=19.99, @category='sports', @title='a simple sawitzki specialist book'
+EXEC CreateBook @isbnString='20-11-12-14', @price=19.99,  @title='a simple sawitzki book'
+EXEC CreateBook @isbnString='20-11-12-15', @price=19.99, @subject='sports', @year=10, @title='a simple sawitzki school book'
+EXEC CreateBook @isbnString='20-11-12-16', @price=19.99, @category='sports', @title='a sophisticated sawitzki specialist book'
 
 select * from PUBLISHING_BOOKS where isbn1 = 20
 select * from PUBLISHING_SCHOOLBOOK
