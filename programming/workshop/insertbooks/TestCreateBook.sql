de59a22fe5ea5e274fc7b452ef93d75aa91c7366
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-04 15:02:10 +0100

    trigger step 1

--- programming/workshop/insertbooks/TestCreateBook.sql
+++ programming/workshop/insertbooks/TestCreateBook.sql
@@ -1,11 +1,11 @@
 use publishing
-DELETE FROM PUBLISHING_SCHOOLBOOK where subject = 'sports'
-DELETE FROM PUBLISHING_SPECIALISTBOOK where category = 'sports'
+DELETE FROM PUBLISHING_SCHOOLBOOK where subject = 'sawitzki'
+DELETE FROM PUBLISHING_SPECIALISTBOOK where category = 'sawitzki'
 DELETE FROM PUBLISHING_BOOKS where isbn1 = 20
 
 EXEC CreateBook @isbnString='20-11-12-14', @price=19.99,  @title='a simple sawitzki book'
-EXEC CreateBook @isbnString='20-11-12-15', @price=19.99, @subject='sports', @year=10, @title='a simple sawitzki school book'
-EXEC CreateBook @isbnString='20-11-12-16', @price=19.99, @category='sports', @title='a sophisticated sawitzki specialist book'
+EXEC CreateBook @isbnString='20-11-12-15', @price=19.99, @subject='sawitzki', @year=10, @title='a simple sawitzki school book'
+EXEC CreateBook @isbnString='20-11-12-16', @price=19.99, @category='sawitzki', @title='a sophisticated sawitzki specialist book'
 
 select * from PUBLISHING_BOOKS where isbn1 = 20
 select * from PUBLISHING_SCHOOLBOOK
