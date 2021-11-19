6b49a02048281d2f2caee0398c9ef6ae97e379c6
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-19 10:16:49 +0100

    introduce books

--- projects/publishing/impl/publishing.js
+++ projects/publishing/impl/publishing.js
@@ -2,3 +2,9 @@
 //{} = JSON, Objekt-Literal
 var addressInMunich = {city: 'München', street: 'Marienplatz'}
 console.log(addressInMunich.city)
+
+var book1 = {isbn: "ISBN1", title: "JavaScript", price: 19.99}
+var book2 = {isbn: "ISBN2", title: "C#", price: 39.99}
+
+console.log(book1.price)
+console.log(book2.price)
