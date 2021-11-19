eb4a51890d3c725f4895c879fa3212b7171a6744
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-19 14:24:22 +0100

    introduce mathods

---
+++ javascript/methods.js
@@ -0,0 +1,10 @@
+var lastname = "Horst"
+console.log(lastname.length)
+console.log(lastname.indexOf('H'))
+console.log(lastname.charAt(2))
+
+var z = 42
+console.log(z.toExponential())
+
+
+console.log(3 > 5)
