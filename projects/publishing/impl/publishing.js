2ffa8757c5eb9223ed8d9bf5e3297b9f3e08139a
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-19 10:27:37 +0100

    extend publishing.js

--- projects/publishing/impl/publishing.js
+++ projects/publishing/impl/publishing.js
@@ -8,3 +8,7 @@ var book2 = {isbn: "ISBN2", title: "C#", price: 39.99}
 
 console.log(book1.price)
 console.log(book2.price)
+
+var bookTitle = "Java"
+var book3 = {isbn: "ISBN2", title: bookTitle, price: 39.99}
+console.log(book3.title)
