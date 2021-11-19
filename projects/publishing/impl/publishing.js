fff3b9815e1c3a40445f03c1aaad4e8d209843af
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-19 11:10:35 +0100

    add publisher

--- projects/publishing/impl/publishing.js
+++ projects/publishing/impl/publishing.js
@@ -1,14 +1,15 @@
 //anlegen einer Adresse
 //{} = JSON, Objekt-Literal
 var addressInMunich = {city: 'München', street: 'Marienplatz'}
-console.log(addressInMunich.city)
+var addressInBerlin = {city: 'Berlin', street: 'Alexanderplatz'}
 
 var book1 = {isbn: "ISBN1", title: "JavaScript", price: 19.99}
 var book2 = {isbn: "ISBN2", title: "C#", price: 39.99}
 
-console.log(book1.price)
-console.log(book2.price)
+var publisher1 = {name: "Springer", address: addressInBerlin}
 
-var bookTitle = "Java"
-var book3 = {isbn: "ISBN2", title: bookTitle, price: 39.99}
-console.log(book3.title)
+
+//gibt aus den Namen des publisher1
+console.log(publisher1.name)
+//gibt aus die Stadt des publisher1
+console.log(publisher1.address.city)
