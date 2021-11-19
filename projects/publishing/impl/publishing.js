7f995e8df9b58c5806d992e2cf6c7c05eebf73a8
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-19 13:26:15 +0100

    introduce books list

--- projects/publishing/impl/publishing.js
+++ projects/publishing/impl/publishing.js
@@ -7,17 +7,27 @@ addressInBerlin.city = 'Berlin'
 addressInBerlin.street = 'Alexanderplatz'
 
 
-var book1 = {}
-book1.isbn = "ISBN1"
-book1.title="JavaScript"
-book1.price = 19.99
+var jsBook = {}
+jsBook.isbn = "ISBN1"
+jsBook.title="JavaScript"
+jsBook.price = 19.99
 
-var book2 = {}
-book2.isbn = "ISBN2"
-book2.title="C#"
-book2.price = 39.99
+var cBook = {}
+cBook.isbn = "ISBN2"
+cBook.title="C#"
+cBook.price = 39.99
 
-var publisher1 = {}
-publisher1.name = "Springer"
-publisher1.address =  addressInBerlin
+var springer = {}
+springer.name = "Springer"
+springer.address =  addressInBerlin
 
+jsBook.publisher = springer
+cBook.publisher = springer
+
+springer.books = [] //Zuweisung einer leeren Liste
+springer.books[0] = jsBook
+springer.books[1] = cBook
+
+
+console.log(springer)
+console.log(springer.books.length)
