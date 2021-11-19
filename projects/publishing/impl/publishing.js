152d707e7b8a747e6af8016d98abc8976e9620e5
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-19 13:41:37 +0100

    add authors

--- projects/publishing/impl/publishing.js
+++ projects/publishing/impl/publishing.js
@@ -11,11 +11,14 @@ var jsBook = {}
 jsBook.isbn = "ISBN1"
 jsBook.title="JavaScript"
 jsBook.price = 19.99
+jsBook.authors = []
 
 var cBook = {}
 cBook.isbn = "ISBN2"
 cBook.title="C#"
 cBook.price = 39.99
+cBook.authors = []
+
 
 var springer = {}
 springer.name = "Springer"
@@ -28,6 +31,26 @@ springer.books = [] //Zuweisung einer leeren Liste
 springer.books[0] = jsBook
 springer.books[1] = cBook
 
+meier = {}
+meier.lastname = "Meier"
+meier.firstname = "Horst"
+meier.books = []
+meier.books[0] = jsBook
+
+
+schneider = {}
+schneider.lastname = "Schneider"
+schneider.firstname = "Hugo"
+schneider.books = []
+
+schneider.books[0] = jsBook
+schneider.books[1] = cBook
+
+jsBook.authors[0] = meier
+jsBook.authors[1] = schneider
+
+cBook.authors[0] = schneider
+
 
 console.log(springer)
-console.log(springer.books.length)
+console.log(jsBook)
