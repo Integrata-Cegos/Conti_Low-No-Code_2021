aeefa2d602db7abc379871982d6b017465d0031b
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-26 12:11:41 +0100

    fix publishingService, add isbn to result

--- projects/publishing/publishingService.js
+++ projects/publishing/publishingService.js
@@ -18,6 +18,7 @@ try {
   const result = await sql.query(`select title from PUBLISHING_BOOKS where isbn1=${isbns[0]} and isbn2 = ${isbns[1]} and isbn3 = ${isbns[2]} and isbn4 = ${isbns[3]}`)
   
   let book = result.recordset[0]
+  book.isbn = isbn
   sql.close()
   return book
  } catch (err) {
