4b1146186976f34f5481924298c0d482f24cb3a9
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-26 11:49:26 +0100

    finish implement findBookByIsbn

--- projects/publishing/publishingService.js
+++ projects/publishing/publishingService.js
@@ -13,13 +13,19 @@ const sqlConfig = {
 
 async function findBookByIsbn(isbn){
 try {
+  let isbns = parseIsbn(isbn)
   await sql.connect(sqlConfig)
-  const result = await sql.query(`select title from PUBLISHING_BOOKS where isbn1=1 and isbn2 = 10 and isbn3 = 12 and isbn4 = 42`)
-  return result.recordset[0]
+  const result = await sql.query(`select title from PUBLISHING_BOOKS where isbn1=${isbns[0]} and isbn2 = ${isbns[1]} and isbn3 = ${isbns[2]} and isbn4 = ${isbns[3]}`)
+  
+  let book = result.recordset[0]
+  sql.close()
+  return book
  } catch (err) {
     console.log(err)
 }}
 
-findBookByIsbn()
+function parseIsbn(isbn){
+    return isbn.split('-').map((isbn) => parseInt(isbn))
+}
 
 exports.findBookByIsbn = findBookByIsbn
