d13eb316867ecffeafb717b561fc1e5bb4adebec
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-26 11:38:27 +0100

    start implement findBookByIsbn

--- projects/publishing/publishingService.js
+++ projects/publishing/publishingService.js
@@ -1,5 +1,25 @@
-function findBookByIsbn(isbn){
-    //TODO impl
-}
+const sql = require('mssql')
+
+const sqlConfig = {
+    user: 'sa',
+    password: 'javacream123!',
+    database: 'publishing',
+    server: 'h2908727.stratoserver.net',
+    options: {
+      trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+  }
+
+
+async function findBookByIsbn(isbn){
+try {
+  await sql.connect(sqlConfig)
+  const result = await sql.query(`select title from PUBLISHING_BOOKS where isbn1=1 and isbn2 = 10 and isbn3 = 12 and isbn4 = 42`)
+  return result.recordset[0]
+ } catch (err) {
+    console.log(err)
+}}
+
+findBookByIsbn()
 
 exports.findBookByIsbn = findBookByIsbn
--- projects/publishing/publishingService.test.js
+++ projects/publishing/publishingService.test.js
@@ -1,8 +1,8 @@
 let publishingService = require('./publishingService')
 
 describe("publishingService", () => {
-    test('book with isbn 1-10-12-42 is found', () => {
-        let book = publishingService.findBookByIsbn('1-10-12-42')
+    test('book with isbn 1-10-12-42 is found', async () => {
+        let book = await publishingService.findBookByIsbn('1-10-12-42')
         expect(book.title).toBe('Test')
     }) 
 })
